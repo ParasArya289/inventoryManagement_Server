8d6cc2801293e4d5422d84b9a4e32bddbaaa19be
ParasArya289 <82272875+ParasArya289@users.noreply.github.com>
2023-10-13 05:56:07 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Inventory Management")
+})
+
+app.use((err,req,res,next)=>{
+  res.status(500).json({error:"Something Went wrong!!"})
+})
+
+app.use((req,res)=>{
+  res.status(404).json({error:"Requested API endpoint does not exist"})
+})
+
+app.listen(3000, () => {
+  console.log("Server started")
+})   
