325f925aeeb47244d314476bb25319e285d89e78
ParasArya289 <82272875+ParasArya289@users.noreply.github.com>
2023-10-13 06:25:22 +0000

    feat: setup inventory router

---
+++ Routers/inventory.router.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const inventoryRouter = express.Router();
+// const {} = require("../Controllers/exercise.controller.js");
+
+// inventoryRouter.get("/exercises",getAllExercisesController)
+
+module.exports = inventoryRouter;
