1b08c7ef3ce91882190bf9a177407ff54d35519a
Paras Arya <82272875+ParasArya289@users.noreply.github.com>
2024-02-28 17:00:10 +0000

    fix: changed sale model

--- Models/sale.model.js
+++ Models/sale.model.js
@@ -1,16 +1,23 @@
 const mongoose = require("mongoose");
 
-const saleSchema = new mongoose.Schema({
-  description: {
-    type:String,
-    required:true
+const saleSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    quantity: {
+      type: Number,
+      required: true,
+    },
   },
-  amount: {
-    type:Number,
-    required:true
-  }
-},{timestamps:true})
+  { timestamps: true },
+);
 
-const Sale = mongoose.model("Sale",saleSchema);
+const Sale = mongoose.model("Sale", saleSchema);
 
 module.exports = Sale;
